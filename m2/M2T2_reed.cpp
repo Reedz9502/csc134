4111c9520fad502c065ad0cd7291c80ad9918126
Reedz9502 <Reedz9502@student.faytechcc.edu>
2026-09-14 15:19:37 +0000

    finale

--- m2/M2T2_reed.cpp
+++ m2/M2T2_reed.cpp
@@ -24,8 +24,19 @@ int main() {
     tax_rate   = 0.08; // 8%
 
     // PROCESSING
+    // tax $ is the meal $ times the tax rate
+    tax_amount  = meal_price * tax_rate;
+    total       = meal_price + tax_amount;
 
     // OUTPUT
+    // TODO: Print like a receipt
+    string line = "-----------------------------------------";
+    cout << line << endl;
+    cout << meal_name << " " << meal_price << endl;
+    cout << " tax: " << " " << tax_amount << endl;
+    cout << line << endl;
+    cout << "Total: " << total << endl;
+    cout << "Thank You Come Again" <<  endl << endl;
 
     return 0; // no errors
 }
