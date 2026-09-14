d9a8eb274092afd8e3b44aa3be55002d5bcb85ff
Reedz9502 <Reedz9502@student.faytechcc.edu>
2026-09-14 14:46:55 +0000

    almost there

---
+++ m2/M2T2_reed.cpp
@@ -0,0 +1,31 @@
+// CSC 134
+// M2T2 - Receipt Calculator
+// Reed, Z
+// 9/14/26
+// Build a receipt that looks like a receipt
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // DECLARE VARIABLES
+    string meal_name;
+    double meal_price;          // $
+    double tax_rate;            // Percent
+    double tax_amount;          // $
+    double total;               // $, meal + tax
+
+    // INPUT
+    // Right now, nothing. They pick exactly one sandwich.
+    // For now, hard code some values
+    meal_name  = "Hot Dog"; // I picked my own because i wanted
+    meal_price = 10.99;
+    tax_rate   = 0.08; // 8%
+
+    // PROCESSING
+
+    // OUTPUT
+
+    return 0; // no errors
+}
